c7135c13942504b62d75990fb0698be6d9af8816
강하윤 <khydino@naver.com>
2025-08-21 15:26:15 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        String[] answer = {};
+        int cnt = 0; 
+        for(int i=0; i<todo_list.length; i++){
+            if(finished[i]==false)
+                cnt++;
+        }
+        answer = new String[cnt];
+        cnt=0;
+        for(int i=0; i<todo_list.length; i++){
+            if(finished[i]==false){
+                answer[cnt] = todo_list[i];
+                cnt++;
+            }
+        }
+        return answer;
+    }
+}
