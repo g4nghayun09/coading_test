59f89432450ac480d3c029eabd5a4b01571c32d0
강하윤 <khydino@naver.com>
2025-09-17 22:54:07 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 168 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        if(a>b)
+            System.out.print(">");
+        if(a<b)
+            System.out.print("<");
+        if(a==b)
+            System.out.print("==");
+    }
+}
