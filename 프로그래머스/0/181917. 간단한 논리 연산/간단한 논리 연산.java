4289cf06befffdf4615acdc1e39db95f076e5bb7
강하윤 <khydino@naver.com>
2026-01-08 22:38:15 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = true;
+        if(x1 == true || x2 == true){
+            if(x3 == true || x4 == true){
+                answer = true;
+            } else{
+                answer = false;
+            }
+        }else{
+            answer = false;
+        }
+        return answer;
+    }
+}
