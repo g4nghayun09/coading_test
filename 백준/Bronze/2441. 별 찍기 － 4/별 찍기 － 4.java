8951db739b411532bbb78e7e773a54799069364a
강하윤 <khydino@naver.com>
2026-03-18 09:18:41 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 308 ms, Memory: 23268 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+
+        for(int i=num; i>0; i--) {
+            for(int k=num; k>i; k--) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
