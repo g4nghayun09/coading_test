74fe12babff02ebb01ac8b03fbd1836c382a5a14
강하윤 <201350428+g4nghayun09@users.noreply.github.com>
2026-03-27 14:09:04 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 372 ms, Memory: 19068 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for(int i=num; i>=0; i--) {
+            for(int k=num-i; k>0; k--) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
