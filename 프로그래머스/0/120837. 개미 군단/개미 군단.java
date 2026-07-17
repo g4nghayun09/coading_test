4855a8d3d8e0786300707f30f63d1647edb314a2
강하윤 <khydino@naver.com>
2026-07-17 15:17:09 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -2,14 +2,11 @@ class Solution {
     public int solution(int hp) {
         int answer = 0;
         int[] attack = new int[3];
-        attack[0] = 5;
-        attack[1] = 3;
-        attack[2] = 1;
         int cnt = 0;
         int temp; 
-        for(int i=0; i<3; i++){
-            answer += hp/attack[i];
-            hp %= attack[i];
+        for(int i=5; i>0; i-=2){
+            answer += hp/i;
+            hp %= i;
         }    
         return answer;
     }
